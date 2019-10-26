427808bef6cbda439c24fc7a301b7771c6535ba3
limatheusm <limatheusm@gmail.com>
2019-10-26 19:50:01 -0300

    add initial routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
